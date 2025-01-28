c2e2ddd222dc48573289e4d0dbfc85dfd01327e1
Horacio <lachocaniz7@gmail.com>
2025-01-28 17:04:46 -0600

    Subir pagina

---
+++ README.md
@@ -0,0 +1,30 @@
+# 🔗 Link in bio
+
+### Share links to all your profiles and websites in one place.
+
+## 🌐 [Demo](https://kaiqcorrea.github.io/)
+
+## 📷 Preview
+
+### Desktop
+![Desktop](https://raw.githubusercontent.com/kaiqcorrea/kaiqcorrea.github.io/main/images/Desktop.png)
+### Mobile
+![Mobile](https://raw.githubusercontent.com/kaiqcorrea/kaiqcorrea.github.io/main/images/Mobile.png)
+
+## 💭 About The Project
+
+I am a beginner in web development and recently challenged myself to create my own version of the Linktree website. I am excited to share the outcome of this endeavor, which also happens to be my first open-source project. While working on it, I drew inspiration from two other websites to guide my design. Despite my foundational knowledge of HTML, I strived to achieve the best possible result. I look forward to further enhancing my skills as I continue to learn programming, as I believe this will lead to even greater improvements in my future projects.
+
+## 💡 My inspirations
+
+| Name | Description |
+| --- | --- |
+| [`@johnggli`](https://github.com/johnggli/linktree) | An open source project |
+
+| Name | Description |
+| --- | --- |
+| [`Monocard`](https://monocard.com.br/) | A Brazilian company that offers products that leads to a personalized page in the linktree style. |
+
+## 📜 License
+
+This project is licensed under the [MIT](https://github.com/nhn/tui.editor/blob/master/LICENSE). See the [LICENSE](https://github.com/kaiqcorrea/link-in-bio/blob/main/LICENSE) file for more information.
---
+++ script.js
@@ -0,0 +1,129 @@
+/* SCRIPT THEME */
+const $html = document.querySelector("html");
+const themeToggle = document.getElementById("theme-toggle");
+
+function toggleTheme() {
+  $html.classList.toggle("light-mode");
+  updateLocalStorage();
+  updateIcons();
+  updateStarsVisibility();
+  updateBackground();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem(
+    "themePreference",
+    $html.classList.contains("light-mode") ? "light" : "dark"
+  );
+}
+
+function loadThemePreference() {
+  const themePreference = localStorage.getItem("themePreference");
+  if (themePreference === "light") {
+    $html.classList.add("light-mode");
+  }
+}
+
+loadThemePreference();
+
+function updateIcons() {
+  const moonIcon = document.querySelector(".fa-moon");
+  const sunIcon = document.querySelector(".fa-sun");
+
+  if ($html.classList.contains("light-mode")) {
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "inline";
+  } else {
+    moonIcon.style.display = "inline";
+    sunIcon.style.display = "none";
+  }
+}
+
+updateIcons();
+updateStarsVisibility();
+updateBackground();
+
+function updateStarsVisibility() {
+  const stars1 = document.getElementById("stars1");
+  const stars2 = document.getElementById("stars2");
+  const stars3 = document.getElementById("stars3");
+
+  if ($html.classList.contains("light-mode")) {
+    stars1.style.display = "none";
+    stars2.style.display = "none";
+    stars3.style.display = "none";
+  } else {
+    stars1.style.display = "block";
+    stars2.style.display = "block";
+    stars3.style.display = "block";
+  }
+}
+
+function updateBackground() {
+  const body = document.body;
+
+  if ($html.classList.contains("light-mode")) {
+    body.style.background = 'url("./images/cloud.svg")';
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "bottom center, 50%, 50%";
+  } else {
+    body.style.background =
+      "radial-gradient(ellipse at bottom, var(--body-gradient1) 0%, var(--body-gradient2) 100%)";
+  }
+}
+
+themeToggle.addEventListener("click", toggleTheme);
+
+/* SCRIPT DOWNLOAD VCARD */
+let timeoutId;
+
+function showToast() {
+  const link = document.querySelector(".add");
+  link.removeEventListener("click", showToast);
+
+  const alert = document.querySelector(".alert");
+  alert.classList.add("fade-in");
+  alert.classList.add("dsp");
+
+  clearToastTimeout();
+
+  timeoutId = setTimeout(() => {
+    hideToast();
+    link.addEventListener("click", showToast);
+  }, 8000);
+}
+
+function hideToast() {
+  const alert = document.querySelector(".alert");
+  alert.classList.remove("fade-in");
+  alert.classList.add("fade-out");
+  setTimeout(() => {
+    alert.classList.remove("fade-out");
+    alert.classList.remove("dsp");
+  }, 800);
+}
+
+function closeToast() {
+  clearToastTimeout();
+  const alert = document.querySelector(".alert");
+  alert.classList.remove("fade-in");
+  alert.classList.add("fade-out");
+  setTimeout(() => {
+    alert.classList.remove("fade-out");
+    alert.classList.remove("dsp");
+  }, 800);
+}
+
+function clearToastTimeout() {
+  clearTimeout(timeoutId);
+}
+
+/* SCRIPT GOOGLE ANALYTICS */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-MFGYPJ17VK");
